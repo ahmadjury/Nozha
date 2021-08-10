9880b486ea6a45fa55327895ec226d7879fcac2d
ahmad jury <ahmadjury12@gmail.com>
2021-08-10 22:57:46 +0300

    push2

--- server/controllers/resort.controller.js
+++ server/controllers/resort.controller.js
@@ -1,4 +1,4 @@
-const Resort = require('../models/school.model')
+const Resort = require('../models/resort.model')
 const School = require('../models/school.model')
 
 	module.exports.findAllResorts = (req, res) => {
@@ -7,30 +7,46 @@ const School = require('../models/school.model')
 			.catch(err => res.json({ message: 'Something went wrong', error: err }));
 	}
 	 
-	module.exports.findOneSingleResort = (req, res) => {
-		Resort.findOne({ _id: req.params.id })
-			.then(oneSingleResort => res.json({ oneSingleResort}))
-			.catch(err => res.json({ message: 'Something went wrong', error: err }));
-	}
+	// module.exports.findOneSingleResort = (req, res) => {
+	// 	Resort.findOne({ _id: req.params.id })
+	// 		.then(oneSingleResort => res.json({ oneSingleResort}))
+	// 		.catch(err => res.json({ message: 'Something went wrong', error: err }));
+	// }
 	 
-	module.exports.createNewResort = (req, res) => {
-		Resort.create(req.body)
-			.then(newlyCreatedResort => res.json(newlyCreatedResort))
-			.catch(err => res.json({ message: 'Something went wrong', error: err }));
-	}
+	// module.exports.createNewResort = (req, res) => {
+	// 	Resort.create(req.body)
+	// 		.then(newlyCreatedResort => res.json(newlyCreatedResort))
+	// 		.catch(err => res.json({ message: 'Something went wrong', error: err }));
+	// }
 	 
-	module.exports.updateExistingResort = (req, res) => {
-		Resort.findOneAndUpdate(
-			{ _id: req.params.id },
+	 
+	// module.exports.deleteAnExistingResort = (req, res) => {
+	// 	Resort.deleteOne({ _id: req.params.id })
+	// 		.then(result => res.json({ result: result }))
+	// 		.catch(err => res.json({ message: 'Something went wrong', error: err }));
+	// }
+
+    module.exports.updateExistingSchool = (req, res) => {
+		School.findOneAndUpdate(
+			{ _id: req.params.id1 },
 			req.body,
 			{ new: true, runValidators: true }
+            
 		)
-			.then(updatedResort => res.json({ updatedResort }))
-			.catch(err => res.json({ message: 'Something went wrong', error: err }));
-	}
-	 
-	module.exports.deleteAnExistingResort = (req, res) => {
-		Resort.deleteOne({ _id: req.params.id })
-			.then(result => res.json({ result: result }))
-			.catch(err => res.json({ message: 'Something went wrong', error: err }));
-	}
+        .then(school=> {
+            Resort.findOneAndUpdate({'_id':req.params.id2},{ 
+                $push:{schools: school}
+             }).catch(err => res.json(err));
+             return res.json(school)
+        })
+        .then(resort=> {
+            School.findOneAndUpdate({'_id':req.params.id1},{ 
+                $push:{resorts: resort}
+             }).catch(err => res.json(err));
+             return res.json(resort)
+        })
+
+        .catch(err => res.json(err));
+    }
+
+	
